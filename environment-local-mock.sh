e92fe069387cb310d4bbdf19f1419380e2d91296
matthew-trump <matthew.trump@gmail.com>
2019-03-29 13:04:50 -0700

    adding local mock environment

---
+++ environment-local-mock.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+export ENVIRONMENT="local-mock"
+export CLOUD_SQL_INSTANCE_IDENTIFIER=""
+export ACCESSOR_PATH=""
+
+export SQL_DB_USERNAME=""
+export SQL_DB_PASSWORD=""
+export SQL_DB_DATABASE=""
+
+export SECRET_KEY: ""
