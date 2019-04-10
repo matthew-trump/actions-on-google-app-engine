47b17279d5e57743f7ba645640472d8291f45b2f
matthew-trump <matthew.trump@gmail.com>
2019-04-09 19:42:20 -0700

    new env for new app

---
+++ environment-quizzes.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+export ENVIRONMENT="local"
+export PORT=8083
+export CLOUD_SQL_INSTANCE_IDENTIFIER=""
+export ACCESSOR_PATH="../../../data-accessor"
+export CONFIG_SCHEMA_PATH="../../config-quizzes.json"
+
+export SQL_DB_USERNAME="root"
+export SQL_DB_PASSWORD="eagle1"
+export SQL_DB_DATABASE="trivia_assistant_1"
+
+export ADMIN_USERNAME="admin"
+export ADMIN_PASSWORD="BjPBdMnC5qd2j55C"
+export ADMIN_SESSION_EXPIRY_IN_SECONDS=2592000
+
+export SECRET_KEY_STAGING="1B24A72B14D94AFBB27D94E384FDC"
+export LOADER_TIMER_INTERVAL_SECONDS=1800
+
+
