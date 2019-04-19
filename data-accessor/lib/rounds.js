f17592e513e35eb4a9bb7b7a9db289c1900c63ab
matthew-trump <matthew.trump@gmail.com>
2019-04-18 19:26:07 -0700

    fix bugs in rounds object

--- data-accessor/lib/rounds.js
+++ data-accessor/lib/rounds.js
@@ -1,63 +1,57 @@
 const DataAccessor = require('./accessor');
 
-const schema = DataAccessor.getSchema();
-
-const ROUND_FIELD = schema.round && schema.round.name ? schema.round.name : 'round';  //quiz
-const SHOWN_FIELD = schema.round && schema.round.shown ? schema.round.shown : 'shown';  //asked
-
 const ALLOW_USER_STORAGE_ACCESS = process.env.ALLOW_USER_STORAGE_ACCESS || 0;
 const SESSION_NO_REPEAT_ENTRIES = process.env.SESSION_NO_REPEAT_ENTRIES > 0;
 
 const rounds = class {
-    constructor() {
-    }
+    constructor() { }
+
     async startRound(conv, options = {}) {
         let excluded = null;
-        let cacheInstance = null;
         if (options.accessUserStorage && ALLOW_USER_STORAGE_ACCESS) {
-            if (conv.user && conv.user.storage[SHOWN_FIELD]) {
-                excluded = conv.user.storage[SHOWN_FIELD];
+            if (conv.user && conv.user.storage.shown) {
+                excluded = conv.user.storage.shown;
             }
         } else if (SESSION_NO_REPEAT_ENTRIES) {
-            excluded = conv.data[ROUND_FIELD] ? conv.data[ROUND_FIELD][SHOWN_FIELD] : [];
+            excluded = conv.data.round ? conv.data.round.shown : [];
         }
         let key;
         let loadOptions;
-        if (conv.data[ROUND_FIELD]
-            && conv.data[ROUND_FIELD].load
-            && conv.data[ROUND_FIELD].load.options) {
+        if (conv.data.round
+            && conv.data.round.load
+            && conv.data.round.load.options) {
 
 
             loadOptions = DataAccessor.getScheduleForeignKeyConfigs().reduce((obj, entityConfig) => {
                 const name = entityConfig.name;
-                if (conv.data[ROUND_FIELD].load.options[name]) {
-                    obj[name] = parseInt(conv.data[ROUND_FIELD].load.options[name]);
+                if (conv.data.round.load.options[name]) {
+                    obj[name] = parseInt(conv.data.round.load.options[name]);
                 } else {
                     obj[name] = entityConfig.default;
                 }
                 return obj;
             }, {
-                    number: conv.data[ROUND_FIELD].load.options.number || DataAccessor.getScheduleFieldConfig('number').default,
-                    pool: conv.data[ROUND_FIELD].load.options.pool || DataAccessor.getScheduleFieldConfig('pool').default
+                    number: conv.data.round.load.options.number || DataAccessor.getScheduleFieldConfig('number').default,
+                    pool: conv.data.round.load.options.pool || DataAccessor.getScheduleFieldConfig('pool').default
                 });
 
             key = DataAccessor.getScheduleKey(loadOptions);
-            delete conv.data[ROUND_FIELD].load;
+            delete conv.data.round.load;
 
         } else {
-            cacheInstance =
-                key = DataAccessor.currentKey;
+
+            key = DataAccessor.currentKey;
         }
 
         await this.ensureLoaded({
-            data: { [ROUND_FIELD]: { key: key } }
+            data: { round: { key: key } }
         });
         const pObj = DataAccessor.scheduleItemCache[key];
 
         if (pObj) {
             const selected = DataAccessor.selectPoolEntries(pObj, excluded);
-            conv.data[ROUND_FIELD] = DataAccessor.generateRound(pObj, selected);
-            conv.data[ROUND_FIELD].key = key;
+            conv.data.round = DataAccessor.generateRound(pObj, selected);
+            conv.data.round.key = key;
 
             const items = selected.map((id) => {
                 return DataAccessor.entityCacheMap[pObj.entity]["" + id + ""]
@@ -65,15 +59,16 @@ const rounds = class {
                 return this.getItemFromEntity(pObj, obj);
             });
             //return all of the items here, up front
+            conv.data.round.items = items.map(item => item.id);
             return Promise.resolve(items);
         } else {
             //bad key: unable to parse load options from key
             console.log("ERROR BAD KEY", key);
             return Promise.reject("ERROR BAD KEY");
         }
+
     }
     getItemFromEntity(pObj, entity) {
-        console.log("ENTITY")
         const item = Object.assign({}, entity.item);
         Object.keys(pObj.foreignKeys).map(plural => {
 
@@ -90,18 +85,19 @@ const rounds = class {
         })
         return item;
     }
-    async getNextItem(conv, options = {}) {
-        const index = typeof options.index !== 'undefined' ? options.index : conv.data[ROUND_FIELD].index;
-        const plural = conv.data[ROUND_FIELD].entity;
-        const id = conv.data[ROUND_FIELD].items[index];
-        let entity = DataAccessor.entityCacheMap[plural][id];
-        const pObj = DataAccessor.scheduleItemCache[conv.data[ROUND_FIELD].key];
+    async getItem(conv, options = {}) {
+        const index = typeof options.index !== 'undefined' ? options.index : conv.data.round.index;
+
+        const id = conv.data.round.items[index];
+        const pObj = DataAccessor.scheduleItemCache[conv.data.round.key];
+        let entity = DataAccessor.entityCacheMap[pObj.entity][id];
+
         if (!entity) {
             //might occur in edge case of load distributed schedule rollover
             //scheduleItemCache should have been instantiated at initial startup
-            let pObj = DataAccessor.scheduleItemCache[conv.data[ROUND_FIELD].key];
+            let pObj = DataAccessor.scheduleItemCache[conv.data.round.key];
             await DataAccessor.loadPoolEntities(pObj, {});
-            entity = DataAccessor.entityCacheMap[plural][id];
+            entity = DataAccessor.entityCacheMap[pObj.entity][id];
             if (!entity) {
                 //this should never happen.
                 console.log("ERROR unable to load ENTRY #354-B. RETURNINNG EMPTY ITEM", id, index);
@@ -117,18 +113,18 @@ const rounds = class {
         return
     }
     async setLatest(conv) {
-        conv.data[ROUND_FIELD].latest = latest;
+        conv.data.round.latest = latest;
     }
     async getNumEntries(conv) {
 
-        if (conv.data[ROUND_FIELD]) {
-            return conv.data[ROUND_FIELD].items.length;
+        if (conv.data.round) {
+            return conv.data.round.items.length;
         } else {
             return null;
         }
     }
     async ensureLoaded(conv) {
-        const key = conv.data[ROUND_FIELD].key;
+        const key = conv.data.round.key;
         let pObj = DataAccessor.scheduleItemCache[key];
         if (pObj) {
             return Promise.resolve(pObj);
