75ce95d6903c44bf055a53b991c9a6ca3422fdec
matthew-trump <matthew.trump@gmail.com>
2019-04-25 10:23:34 -0700

    env for a deluxe version of quizzes

---
+++ environment-quizzes-deluxe.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+export ENVIRONMENT="local"
+export PORT=8085
+export CLOUD_SQL_INSTANCE_IDENTIFIER=""
+export ACCESSOR_PATH="../../../data-accessor"
+export CONFIG_SCHEMA_PATH="../../config-quizzes-deluxe.json"
+
+export SQL_DB_USERNAME="root"
+export SQL_DB_PASSWORD="eagle1"
+export SQL_DB_DATABASE="quizzes-deluxe"
+
+export ADMIN_USERNAME="admin"
+export ADMIN_PASSWORD="BjPBdMnC5qd2j55C"
+export ADMIN_SESSION_EXPIRY_IN_SECONDS=2592000
+
+export SECRET_KEY="1B24A72B14D94AFBB27D94E384FDC"
+export LOADER_TIMER_INTERVAL_SECONDS=0
+
+
