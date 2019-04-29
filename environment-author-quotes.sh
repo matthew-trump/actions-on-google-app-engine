70bbe74cd5692d20e594aa251c2fb228acb079f3
matthew-trump <matthew.trump@gmail.com>
2019-04-29 09:43:31 -0700

    use alt config file for experimentation

--- environment-author-quotes.sh
+++ environment-author-quotes.sh
@@ -3,7 +3,7 @@ export ENVIRONMENT="local"
 export PORT=8082
 export CLOUD_SQL_INSTANCE_IDENTIFIER=""
 export ACCESSOR_PATH="../../../data-accessor"
-export CONFIG_SCHEMA_PATH="../../config-author-quotes.json"
+export CONFIG_SCHEMA_PATH="../../config-author-quotes-1.json"
 
 export SQL_DB_USERNAME="root"
 export SQL_DB_PASSWORD="eagle1"
@@ -15,7 +15,7 @@ export ADMIN_SESSION_EXPIRY_IN_SECONDS=2592000
 
 export SECRET_KEY="/B?E(H+MbQeThWmZq3t6w9z&F)J@Nc"
 
-export LOADER_TIMER_INTERVAL_SECONDS=1800
+export LOADER_TIMER_INTERVAL_SECONDS=0
 
 
 
