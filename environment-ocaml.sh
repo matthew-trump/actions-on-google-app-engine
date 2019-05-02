9770955d087914a1b40923fd7c9bc137dcfc7520
matthew-trump <matthew.trump@gmail.com>
2019-05-02 12:33:47 -0700

    new app for ocaml code samples

---
+++ environment-ocaml.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+export ENVIRONMENT="local"
+export PORT=8087
+export CLOUD_SQL_INSTANCE_IDENTIFIER=""
+export ACCESSOR_PATH="../../../data-accessor"
+export CONFIG_SCHEMA_PATH="../../config-ocaml.json"
+
+export SQL_DB_USERNAME="root"
+export SQL_DB_PASSWORD="eagle1"
+export SQL_DB_DATABASE="ocaml"
+
+export ADMIN_USERNAME="admin"
+export ADMIN_PASSWORD="BjPBdMnC5qd2j55C"
+export ADMIN_SESSION_EXPIRY_IN_SECONDS=2592000
+
+export LOADER_TIMER_INTERVAL_SECONDS=0
+
+
